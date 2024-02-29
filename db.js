24c160ce58e8a68d7c37142c8f377eee5754f5f7
jakhong1r-dev <jahongirabduazimov707@gmail.com>
2024-02-29 18:28:54 +0500

    Namoz vaqtlari

---
+++ db.js
@@ -0,0 +1,14 @@
+const provencie = [
+    "Toshkent",
+    "Farg'ona",
+    "Samarqand",
+    "Xorazm",
+    "Navoiy",
+    "Qashqadaryo",
+    "Surxondaryo",
+    "Andijon",
+    "Namangan",
+    "Jizzax",
+    "Buxoro",
+    "Sirdaryo",
+ ];
---
+++ index.js
@@ -0,0 +1,73 @@
+"use strict";
+
+let baseURL = "https://islomapi.uz/api/present/day";
+let changeRegion = $("#change-region");
+let card = $('.card');
+
+let tong = $('#tong');
+let quyosh = $('#quyosh');
+let peshin = $('#peshin');
+let asr = $('#asr');
+let shom = $('#shom');
+let xufton = $('#xufton');
+
+
+async function namazTime(data) {
+    try {
+        let response = await fetch(baseURL + `?region=${data}`);
+        let result = await response.json();
+        dataNamoz(result);
+    } catch (err) {
+        console.log(err);
+    }
+}
+namazTime("Toshkent")
+
+function dataNamoz(el) {
+    tong.textContent = `${el.times.asr}`
+    quyosh.textContent = `${el.times.quyosh}`
+    peshin.textContent = `${el.times.peshin}`
+    asr.textContent = `${el.times.asr}`
+    shom.textContent = `${el.times.shom_iftor}`
+    xufton.textContent = `${el.times.hufton}`
+}
+
+
+
+function renderData(data) {
+    data.forEach((el) => {
+        let option = document.createElement('option');
+        option.setAttribute('value', `${el}`)
+        option.innerHTML = `
+            ${el}
+        `
+        changeRegion.appendChild(option)
+
+    })
+}
+renderData(provencie)
+
+
+async function findTime(data) {
+    try {
+        let response = await fetch(baseURL + `?region=${data}`)
+        let result = await response.json()
+        renderCardData(result)
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+changeRegion.addEventListener('change', (e) => {
+    findTime(e.target.value)
+})
+
+
+function renderCardData(el) {
+    tong.textContent = `${el.times.asr}`
+    quyosh.textContent = `${el.times.quyosh}`
+    peshin.textContent = `${el.times.peshin}`
+    asr.textContent = `${el.times.asr}`
+    shom.textContent = `${el.times.shom_iftor}`
+    xufton.textContent = `${el.times.hufton}`
+}
